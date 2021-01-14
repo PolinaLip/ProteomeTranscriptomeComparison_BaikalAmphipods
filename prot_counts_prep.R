8eca45df0bce67cb6e8065d69f5982fa0415239d
PolinaLip <polina.lipaeva@gmail.com>
2021-01-14 17:30:45 +0100

    update

--- prot_counts_prep.R
+++ prot_counts_prep.R
@@ -1,4 +1,16 @@
-# counts data preparation
+###########################################
+#### Proteome counts data preparation #####
+###########################################
+
+library(limma)
+library(dplyr)
+library(tidyverse)
+library(ggfortify)
+
+### 0. Choose species
+species <- 'Gla'
+
+### 1. Upload metafile
 meta_upload <- function(path_to_file, species_name) {
   meta <- read.csv(file = path_to_file, sep = '\t')
   meta$measure <- sub('intensity', 'intensity count', meta$measure)
@@ -8,13 +20,19 @@ meta_upload <- function(path_to_file, species_name) {
 }
 path2meta <-
   'labeglo2/MS_results/390/withDBfromRNAspades/wIMBR2/Metadata_Proteus.tsv'
-meta <- meta_upload(path2meta, 'Eve')
 
+meta <- meta_upload(path2meta, species)
+
+### 2. Upload proteinGroups file 
 dir <- 'labeglo2/MS_results/390/withDBfromRNAspades/wIMBR2/protein_groups_eve/'
+dir <- 'labeglo2/MS_results/390/withDBfromRNAspades/wIMBR2/protein_groups_ecy/'
+dir <- 'labeglo2/MS_results/390/withDBfromRNAspades/wIMBR2/protein_groups_gla/'
+
 proteinGroups_file <- 'proteinGroups_wo_cont_more2pept.txt' # take file with proteinGroups with 2 or more peptides quantified
 dat_init <- read.csv(file.path(dir, proteinGroups_file), sep = '\t', header = T, 
                      check.names = F) 
 
+### 3. Choose columns with counts data
 select_data <- function(meta_data, proteinGroups_data){
   dat <- proteinGroups_data[,c('Protein IDs', meta_data$measure)]
   rownames(dat) <- dat$`Protein IDs`
@@ -23,81 +41,120 @@ select_data <- function(meta_data, proteinGroups_data){
   dat[dat == 0] <- NA
   return(dat)
 }
+
 dat_count <- select_data(meta, dat_init)
+
+### 4. Remove samples which are outliers (should be determined in advance) 
+###    or just unwanted one
 dat_count <- dat_count[,!grepl('VrK1_390_3|VrK1_390_4', colnames(dat_count))]
-dat_count <- dat_count[,!grepl('Eve_pool', colnames(dat_count))]
+dat_count <- dat_count[,!grepl('pool', colnames(dat_count))]
 meta <- subset(meta, sample != 'VrK1_390_3' & sample != 'VrK1_390_4' &
-                 !grepl('Eve_pool', sample)
-)
+                 !grepl('pool', sample))
 
-proteins_intensities_no_na <- na.omit(dat_count)
-proteins_intensities_no_na <- as.data.frame(proteins_intensities_no_na)
-data_raw_count <- proteins_intensities_no_na
-boxplot(log2(data_raw_count), col = c(rep(c('red','blue'), each = 9), rep('green', 4)), 
-        notch = TRUE, main = 'RAW data: Exp1 (red), Exp2 (blue), Exp3 (green)',
-        xlab = 'TMT Samples', ylab = 'log2 of Intensity') # Eve, resolution -> 1400/690
-plotDensities(log2(data_raw_count), col = c(rep(c('red','blue'), each = 9), rep('green', 4)), 
-              main = 'Raw data', legend=F) # Eve, resolution -> 800/553
+#proteins_intensities_no_na <- na.omit(dat_count)
+#proteins_intensities_no_na <- as.data.frame(proteins_intensities_no_na)
+#data_raw_count <- proteins_intensities_no_na
 
-exp1_raw_count <- data_raw_count[c(1:8)]
-exp2_raw_count <- data_raw_count[c(9:16)]
-exp3_raw_count <- data_raw_count[c(17:19)]
+### 5. To get rid of observations with all NAs in one of the conditions
+cond <- as.factor(meta$condition)
+ixs <- order(cond)
+dat_count <- dat_count[, ixs]
+cond <- cond[ixs]
 
-target_count <- mean(c(colSums(exp1_raw_count, na.rm = T), 
-                 colSums(exp2_raw_count, na.rm = T),
-                 colSums(exp3_raw_count, na.rm = T)), na.rm = T) # Eve 
-norm_facs_count <- target_count / colSums(exp1_raw_count, na.rm = T)
-exp1_sl_count <- sweep(exp1_raw_count, 2, norm_facs_count, FUN = "*")
-norm_facs_count <- target_count / colSums(exp2_raw_count, na.rm = T)
-exp2_sl_count <- sweep(exp2_raw_count, 2, norm_facs_count, FUN = "*")
-norm_facs_count <- target_count / colSums(exp3_raw_count, na.rm = T)
-exp3_sl_count <- sweep(exp3_raw_count, 2, norm_facs_count, FUN = "*")
+for (level in levels(cond)) {
+  col_ixs <- cond == level
+  dat_count <- dat_count[rowSums(is.na(dat_count[,col_ixs])) < sum(col_ixs),]
+}
 
-data_sl_count <- cbind(exp1_sl_count, exp2_sl_count, exp3_sl_count)
+data_raw_count <- dat_count
 
-boxplot(log2(data_sl_count), col = c(rep(c('red', 'blue'), each = 9), rep('green', 4)), 
-        notch = TRUE, main = "Sample Loading (SL) normalized data: \nExp1 (red), Exp2 (blue), Exp3 (green)",
-        xlab = 'TMT Sample', ylab = 'log2 of Intensity') # Eve, resolution -> 1400/690
-plotDensities(log2(data_sl_count), col = c(rep(c('red', 'blue'), each = 9), rep('green', 4)),
+### 6. Sample loading and IRS normalization
+
+sl_irs_normalization <- function(counts, metafile, species) {
+  if (species == 'Eve'){
+    exp1_raw <- counts[,subset(metafile, experiment == 6)$sample]
+    exp2_raw <- counts[,subset(metafile, experiment == 7)$sample]
+    exp3_raw <- counts[,subset(metafile, experiment == 8)$sample]
+  }
+  else if (species == 'Ecy') {
+    exp1_raw <- counts[,subset(metafile, experiment == 4)$sample]
+    exp2_raw <- counts[,subset(metafile, experiment == 5)$sample]
+  }
+  else if (species == 'Gla') {
+    exp1_raw <- counts[,subset(metafile, experiment == 1)$sample]
+    exp2_raw <- counts[,subset(metafile, experiment == 2)$sample]
+    exp3_raw <- counts[,subset(metafile, experiment == 3)$sample]
+  }
+  if (species == 'Eve' | species == 'Gla') {
+    target <- mean(c(colSums(exp1_raw, na.rm = T), colSums(exp2_raw, na.rm = T), 
+                     colSums(exp3_raw, na.rm = T)),
+                   na.rm = T)
+    norm_facs <- target / colSums(exp1_raw, na.rm = T)
+    exp1_sl <- sweep(exp1_raw, 2, norm_facs, FUN = "*")
+    norm_facs <- target / colSums(exp2_raw, na.rm = T)
+    exp2_sl <- sweep(exp2_raw, 2, norm_facs, FUN = "*")
+    norm_facs <- target / colSums(exp3_raw, na.rm = T)
+    exp3_sl <- sweep(exp3_raw, 2, norm_facs, FUN = "*")
+    
+    data_sl <- cbind(exp1_sl, exp2_sl, exp3_sl)
+    
+    irs <- tibble(rowSums(exp1_sl), 
+                  rowSums(exp2_sl), rowSums(exp3_sl)) 
+    colnames(irs) <- c("sum1", "sum2", "sum3") 
+    irs$average <- apply(irs, 1, function(x) exp(mean(log(x), na.rm = T)))
+    
+    irs$fac1 <- irs$average / irs$sum1 * ncol(exp1_sl) 
+    irs$fac2 <- irs$average / irs$sum2 * ncol(exp2_sl) 
+    irs$fac3 <- irs$average / irs$sum3 * ncol(exp3_sl) 
+    
+    data_irs_count <- exp1_sl * irs$fac1
+    data_irs_count <- cbind(data_irs_count, exp2_sl * irs$fac2)
+    data_irs_count <- cbind(data_irs_count, exp3_sl * irs$fac3)
+  }
+  
+  else if (species == 'Ecy') {
+    target <- mean(c(colSums(exp1_raw, na.rm = T), colSums(exp2_raw, na.rm = T)),
+                   na.rm = T)
+    norm_facs <- target / colSums(exp1_raw, na.rm = T)
+    exp1_sl <- sweep(exp1_raw, 2, norm_facs, FUN = "*")
+    norm_facs <- target / colSums(exp2_raw, na.rm = T)
+    exp2_sl <- sweep(exp2_raw, 2, norm_facs, FUN = "*")
+    
+    data_sl <- cbind(exp1_sl, exp2_sl)
+    
+    irs <- tibble(rowSums(exp1_sl), rowSums(exp2_sl)) 
+    colnames(irs) <- c("sum1", "sum2") 
+    irs$average <- apply(irs, 1, function(x) exp(mean(log(x), na.rm = T)))
+    
+    irs$fac1 <- irs$average / irs$sum1 * ncol(exp1_sl) 
+    irs$fac2 <- irs$average / irs$sum2 * ncol(exp2_sl) 
+    
+    data_irs_count <- exp1_sl * irs$fac1
+    data_irs_count <- cbind(data_irs_count, exp2_sl * irs$fac2)
+  }
+  return(data_irs_count)
+}
+
+data_sl_irs_count <- sl_irs_normalization(data_raw_count, 
+                                          meta, 
+                                          species = species)
+
+boxplot(log2(data_sl_irs_count), notch = TRUE,
+        xlab = 'TMT Sample', ylab = 'log2 of counts') # Eve, resolution -> 1400/690
+plotDensities(log2(data_sl_irs_count), 
               main = "SL normalization", legend = F) # Eve, resolution -> 800/553
 
-plotMDS(log2(data_sl_count), col = c(rep(c('red', 'blue'), each = 9), rep('green', 4)), 
+plotMDS(log2(data_sl_irs_count), col = c(rep(c('red', 'blue'), each = 9), 
+                                     rep('green', 4)), 
         main = "SL clusters group by TMT experiment") # Eve
-data_sl_t_count <- na.omit(data_sl_count) %>% t %>% as.data.frame
-pca_res <- prcomp(data_sl_t_count)
+
+data_sl_irs_count_t <- na.omit(data_sl_irs_count) %>% t %>% as.data.frame
+pca_res <- prcomp(data_sl_irs_count_t)
 meta2 <- meta
 meta2$experiment <- as.factor(meta2$experiment)
 autoplot(pca_res, data=meta2, colour='condition')
 
-irs <- tibble(rowSums(exp1_sl_count), 
-              rowSums(exp2_sl_count), rowSums(exp3_sl_count)) 
-colnames(irs) <- c("sum1", "sum2", "sum3") 
-irs$average <- apply(irs, 1, function(x) exp(mean(log(x), na.rm = T)))
+write.csv(data_sl_irs_count, 
+          file = file.path('labeglo2/proteome_transcr_comparision/norm_counts_Gla_HS_protGroups_withNA.csv'))
 
-irs$fac1 <- irs$average / irs$sum1 * ncol(exp1_sl_count) # Ecy, Eve, Gla
-irs$fac2 <- irs$average / irs$sum2 * ncol(exp2_sl_count) # Ecy, Eve, Gla
-irs$fac3 <- irs$average / irs$sum3 * ncol(exp3_sl_count) # Eve, Gla
-
-data_irs_count <- exp1_sl_count * irs$fac1
-data_irs_count <- cbind(data_irs_count, exp2_sl_count * irs$fac2)
-data_irs_count <- cbind(data_irs_count, exp3_sl_count * irs$fac3)
-
-boxplot(log2(data_irs_count), col = c(rep(c('red', 'blue'), each = 9), rep('green', 4)), 
-        main = "Internal Reference Scaling (IRS) normalized data: \nExp1 (red), Exp2 (blue), Exp3 (green)",
-        xlab = 'TMT Sample', ylab = 'log2 of Intensity', notch = TRUE) # Eve, resolution 1400/690
-
-col_vec <- ifelse(grepl('pool', colnames(data_irs_count)), 'green',
-                  ifelse(grepl('VrK1', colnames(data_irs_count)), 'orange',
-                         ifelse(grepl('Vr1', colnames(data_irs_count)), 'blue', 'red')))
-
-plotMDS(log2(data_irs_count), col = col_vec, main = "IRS clusters do not group by TMTsets anymore")
-
-data_irs_t_count <- na.omit(data_irs_count) %>% t %>% as.data.frame
-pca_res <- prcomp(data_irs_t_count)
-meta2 <- meta
-meta2$experiment <- as.factor(meta2$experiment)
-autoplot(pca_res, data=meta2, colour='condition')
-
-write.csv(data_irs_count, 
-          file = file.path('labeglo2/proteome_transcr_comparision/norm_counts_Eve_HS_protGroups.csv'))
-
+# then -> prot_transcr_conc.R
---
+++ prot_transcr_conc.R
@@ -0,0 +1,179 @@
+library(tidyr)
+library(dplyr)
+library(tibble)
+library(ggplot2)
+library(rstatix)
+
+### Intensity values preparation:
+intensities <- read.csv('labeglo2/proteome_transcr_comparision/norm_counts_Gla_HS_protGroups_withNA.csv')
+rownames(intensities) <- intensities$X
+intensities <- intensities[-1]
+intensities <- log2(intensities)
+meta <- 
+  read.csv(file = 
+             'labeglo2/MS_results/390/withDBfromRNAspades/wIMBR2/Metadata_Proteus.tsv',
+           sep = '\t')
+meta <- meta[grepl('Gla', meta$condition),]
+meta <- subset(meta, sample != 'VrK1_390_3' & sample != 'VrK1_390_4')
+
+intensities <- intensities[,grepl('VrK2|Vr1|CK2|C1|LK2|L1', 
+                                  colnames(intensities))]
+
+pep_annot <- 
+  read.csv('labeglo2/MS_results/390/withDBfromRNAspades/wIMBR2/protein_groups_gla/annot_protein_groups_gla.csv', 
+           sep = '\t',
+           header = T)
+
+prot2annot <- match(rownames(intensities), pep_annot$protein_group)
+sum(is.na(prot2annot))
+intensities$annotation <- pep_annot$annotation[prot2annot]
+
+intens_long <- pivot_longer(intensities %>% rownames_to_column('protein_group'),
+                            starts_with(c('C', 'V', 'L')), names_to = 'sample', 
+                            values_to = 'intensity')
+intens_long$condition <- meta$condition[match(intens_long$sample, meta$sample)]
+
+intens_long <- as.data.frame(intens_long)
+intens_long_mean <- aggregate(intensity ~ protein_group + condition + annotation, 
+                              data = intens_long, median, na.rm = T)
+
+### Counts preparation:
+annot <- read.csv('labeglo2/proteome_transcr_comparision/contigs_whole_annot_Gla.csv', 
+                  sep = '\t')
+dir_transcr <- '~/labeglo2/Transcriptomics/quantification/HS_exp_labeglo1/Gla/counts' # specify path to your samples
+
+files_names <- c('Eve_6C_rep1', 'Eve_6C_rep2', 'Eve_6C_rep3', 'Eve_6C_rep4', 
+                 'Eve_24C_rep1', 'Eve_24C_rep2', 'Eve_24C_rep3', 'Eve_24C_rep4') # specify the names of folders with quant.sf data, Eve
+
+files_names <- c('Ecy_6C_rep1', 'Ecy_6C_rep2', 'Ecy_6C_rep3', 'Ecy_6C_rep4', 
+                 'Ecy_24C_rep1', 'Ecy_24C_rep2', 'Ecy_24C_rep3', 'Ecy_24C_rep4') # specify the names of folders with quant.sf data, Ecy
+
+files_names <- c('Gla_6C_rep1', 'Gla_6C_rep2', 'Gla_6C_rep3', 'Gla_6C_rep4', 
+                 'Gla_24C_rep1', 'Gla_24C_rep2', 'Gla_24C_rep3') # specify the names of folders with quant.sf data, Gla
+
+countFiles <- list.files(dir_transcr, full.names = T)
+countFiles
+counts <- lapply(countFiles, function(countsFile) {
+  f <- file.path(countsFile, 'quant.sf')
+  read.table(f, sep="\t", header=1, row.names = 1, 
+             stringsAsFactors = F, comment.char = "#")
+})
+counts <- lapply(counts, function(countsTable) countsTable[, 3, drop=F])
+
+counts <- do.call(cbind, counts)
+sample_names <- c('24C_rep1', '24C_rep2', '24C_rep3',
+                  '6C_rep1', '6C_rep2', '6C_rep3', '6C_rep4')
+colnames(counts) <- sample_names
+
+contig2annot <- match(rownames(counts), annot$contig)
+counts$transcr_annot <- annot$annot[contig2annot]
+
+counts_long <- pivot_longer(counts %>% rownames_to_column('contig'), 
+                            starts_with(c('2','6')), names_to = 'transc_sample',
+                            values_to = 'counts')
+
+counts_long$condition <- ifelse(grepl('24C', counts_long$transc_sample), 
+                                '24C', '6C')
+counts_long_mean <- aggregate(counts ~ contig + condition + transcr_annot, 
+                              data = counts_long, median, na.rm = T)
+
+### 24C and 6C correlation
+
+intens_long_mean_24C <- subset(intens_long_mean, condition == 'Gla_24C')
+intens_long_mean_24C <- intens_long_mean_24C[-2]
+intens_long_mean_6C <- subset(intens_long_mean, condition == 'Gla_6C_after')
+intens_long_mean_6C <- intens_long_mean_6C[-2]
+
+# choose condition:
+intens_to_analise <- intens_long_mean_24C
+intens_to_analise <- intens_long_mean_6C
+
+intens_to_analise$prot_group_id <- 1:nrow(intens_to_analise)
+proteins_sep <- separate(intens_to_analise, protein_group, ';', 
+                         into=paste0('key', 1:30), 
+                         fill='right')
+proteins_long <- pivot_longer(proteins_sep, starts_with('key'),
+                              names_to = NULL, values_to = 'protein') %>%
+  filter(!is.na(protein))
+
+# make a column with protein names without everything after .p (including .p) 
+proteins_long$protein_clip <- sub('\\.p[0-9]+_...$', '', proteins_long$protein)
+
+# if there are two proteins with the same clipped name take the protein with 
+# maximum intensity value:
+proteins_long <- group_by(proteins_long, protein_clip) %>%
+  slice_max(intensity, n = 1, with_ties = F)
+
+counts_long_mean_24C <- subset(counts_long_mean, condition == '24C')
+counts_long_mean_24C <- counts_long_mean_24C[-2]
+counts_long_mean_6C <- subset(counts_long_mean, condition == '6C')
+counts_long_mean_6C <- counts_long_mean_6C[-2]
+
+# choose condition:
+counts_to_analyse <- counts_long_mean_24C
+counts_to_analyse <- counts_long_mean_6C
+
+joined <- inner_join(counts_to_analyse, proteins_long, 
+                     by = c('contig' = 'protein_clip'))
+
+joined_merged <- joined %>% 
+  group_by(prot_group_id) %>%
+  mutate(max_count = max(counts)) %>% 
+  mutate(transcr_groups = paste0(contig, collapse = ';')) %>%
+  mutate(proteome_groups = paste0(protein, collapse = ';')) %>%
+  ungroup() %>%
+  dplyr::select(!c(contig, counts, protein, transcr_annot)) %>%
+  unique()
+
+ggplot(joined, aes(log2(counts), intensity)) +
+  geom_point(alpha=.5, color='gray70') +
+  geom_smooth() +
+#  geom_point(data=filtered_joined_clip, color='red') +
+#  geom_text_repel(aes(label= geneSymbol),
+#                  data = filtered_joined_clip) +
+#  xlab('log2FC (24°C/6°C) transcriptome') +
+#  ylab('log2FC (24°C/6°C) proteome') +
+  theme_light() +
+  theme(axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
+# this plot will give duplicated values, below is without duplicates
+cor.test(joined_merged$max_count, exp(joined_merged$intensity))
+
+tmp <- filter(proteins_long, grepl('Serpin', annotation))
+ggplot(joined_merged, aes(log2(max_count), intensity)) +
+  geom_point(aes(color=grepl(paste0(tmp$protein_clip, collapse='|'), transcr_groups)), alpha=.5)
+tmp2 <- filter(joined_merged,
+               grepl(paste0(tmp$protein_clip, collapse='|'), transcr_groups))
+
+cor_test_res <- cor.test(joined_merged$max_count, exp(joined_merged$intensity))
+
+joined_merged <- subset(joined_merged, max_count != 0)
+
+ggplot(joined_merged, aes(log2(max_count), intensity)) +
+  geom_point(alpha=.5, color='gray70') +
+  geom_smooth(method='lm') +
+  #  geom_point(data=filtered_joined_clip, color='red') +
+  #  geom_text_repel(aes(label= geneSymbol),
+  #                  data = filtered_joined_clip) +
+  annotate(geom='text', x = -4.5, y = 12, hjust = 0,
+           label = paste0('r2 = ', round(cor_test_res$estimate, 4), '\n',
+                          'p-value ', p_format(cor_test_res$p.value,
+                                                 accuracy = 0.000001))) +
+  xlab('transcipt abundance at 6°C (log2 count)') +
+  ylab('protein abundance at 6°C (log2 count)') +
+  theme_light() +
+  theme(axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12))
+
+ggsave('labeglo2/proteome_transcr_comparision/prot_trans_counts_Gla_6C_withNAs_lm.png',
+       scale = 1.5)
+
+filter(joined, log2(counts) > 10 & intensity < 0.1)
+tmp <- filter(intens_long, grepl('NODE_1149_length_6503_cov_6439', protein_group))
+
+# use Reporter intensity count from proteinGroup.txt
+# if use intensities with all proteins (even with zero inside) -> we can see some positive correlation between proteome and transcriptome
+#ggsave('labeglo2/proteome_transcr_comparision/prot_trans_conc_ProteinsWithZerosAlso.png')
+
+# ! use only 6C from parallel control ! -> and the same applies to lfc comparision of proteome and transcriptome
+
